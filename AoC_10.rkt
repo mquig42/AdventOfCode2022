b7ac06ded2e71de80a529d0cb012f67e8d28417a
mquig42 <mquig42@users.noreply.github.com>
2022-12-10 12:54:20 -0500

    Comments

--- AoC_10.rkt
+++ AoC_10.rkt
@@ -7,11 +7,11 @@
 ;;;What will part 2 bring, I wonder?
 
 ;;;Update: Oh, it's one of these puzzles where the output spells out
-;;;a word in giant latters. Those are always fun. I'll finish in the morning,
+;;;a word in giant letters. Those are always fun. I'll finish in the morning,
 ;;;but it looks like my approach to part 1 of making a vector containing
 ;;;the value of the x register during every cycle will set me up well for part 2
-;;Update: Had to tweak things a bit. Switched from a vector to a list.
-;;It makes part 1 less efficient, but is better for part 2
+;;;Update: Had to tweak things a bit. Switched from a vector to a list.
+;;;It makes part 1 less efficient, but is better for part 2
 #lang racket
 
 (define (read-input file)
@@ -32,9 +32,12 @@
                       (cons x (cons x hist))))))
   (reverse (iter instrs 1 '(0))))
 
+;;Returns pixel value to draw based on x register and h counter
 (define (crt-pixel h x)
   (if (<= (abs (- h x)) 1) #\▓ #\░))
 
+;;Draws all the pixels defined by the sequence of x-register values.
+;;h starts at 0
 (define (draw-screen signals h)
   (cond ((null? signals) (void))
         ((= h 39)
