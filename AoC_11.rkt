3616c7a2bf9d548452e3ba698b53b8626ad09b56
mquig42 <mquig42@users.noreply.github.com>
2022-12-12 15:27:28 -0500

    Day 11 part 1

--- AoC_11.rkt
+++ AoC_11.rkt
@@ -5,12 +5,15 @@
 ;;;A few thoughts: First, it doesn't matter in what order a monkey inspects
 ;;;its things. Everything in the queue will be looked at, one at a time
 ;;;This means I can use a stack instead of a queue.
+;;;Edit: Since the text refers to order mattering, I'm going to use queues
+;;;even if it seems like stacks would also work
 ;;;Second: I might want to use mutable vectors. They'll be faster.
 ;;;If I enclose all the mutable data inside a function, it's still FP, right?
 ;;;Third: I'm scared of part 2. This could be a day where part 2 is just
 ;;;"do part 1 a zillion times", and I'll need to find some math formula
 ;;;because conventional programming won't work on that scale.
 #lang racket
+(require queue)
 
 ;;The functions read-monkey, read-monkeys, operation, and test
 ;;are used for reading input and generating a list of monkeys
@@ -28,13 +31,14 @@
               (read-line file))))
 
 ;;Generates a procedure based on an Operation string from today's input
+;;Includes division by 3
 (define (operation str)
   (let ((parsed (string-split str)))
-    (λ (old) ((if (string=? (second parsed) "*") * +)
-              old
-              (if (string=? (third parsed) "old")
-                  old
-                  (string->number (third parsed)))))))
+    (λ (old) (quotient ((if (string=? (second parsed) "*") * +)
+                        old
+                        (if (string=? (third parsed) "old")
+                            old
+                            (string->number (third parsed)))) 3))))
 
 ;;Generates a procedure from the three Test lines in today's input
 ;;This procedure will take a worry level as input and return a monkey's number
@@ -45,6 +49,43 @@
              (string->number (last (string-split str2)))
              (string->number (last (string-split str3))))))
 
+;;Returns a vector containing each monkey's activity level after n rounds
+;;This function uses mutable vectors and for-each internally,
+;;but does not depend on any external state
+(define (run-n-rounds monkeys rounds)
+  (let ((queues (list->vector (map list->queue (map first monkeys))))
+        (inspects (list->vector (map second monkeys)))
+        (tests (list->vector (map third monkeys)))
+        (activity (make-vector (length monkeys))))
+    (define (monkey-turn n)
+      (cond ((queue-empty? (vector-ref queues n)) (void))
+            (else
+             (define-values (elem new-q) (queue-remove (vector-ref queues n)))
+             (let* ((new-val ((vector-ref inspects n) elem))
+                    (new-idx ((vector-ref tests n) new-val)))
+               (vector-set!
+                queues
+                new-idx
+                (queue-add (vector-ref queues new-idx) new-val))
+               (vector-set! queues n new-q)
+               (vector-set! activity n (+ (vector-ref activity n) 1))
+               (monkey-turn n)))))
+    (for-each (λ (x) (for-each monkey-turn (range (length monkeys))))
+              (range rounds))
+    activity))
+
+;;Makes a queue from a list.
+(define (list->queue lst)
+  (foldl (λ (x acc) (queue-add acc x)) (make-queue) lst))
+
+
 (define input-file (open-input-file "Input11.txt"))
 (define input (read-monkeys input-file))
 (close-input-port input-file)
+
+(display "Part 1: ")
+(foldl * 1
+       (vector->list
+        (vector-take
+         (vector-sort
+          (run-n-rounds input 20) >) 2)))
