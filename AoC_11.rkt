d587fb092035dfba732514b2c20d4261ba7c5454
mquig42 <mquig42@users.noreply.github.com>
2022-12-12 16:28:21 -0500

    Day 11 part 2

--- AoC_11.rkt
+++ AoC_11.rkt
@@ -12,6 +12,14 @@
 ;;;Third: I'm scared of part 2. This could be a day where part 2 is just
 ;;;"do part 1 a zillion times", and I'll need to find some math formula
 ;;;because conventional programming won't work on that scale.
+
+;;;Update: it was actually 10,000 times. The difficulty was that the
+;;;"divide by 3" step was removed, so the numbers got too big.
+;;;Solved that by modding by the product of all the test divisors
+;;;(which I admit I got from the subreddit). My first attempt probably would
+;;;have found the answer eventually. I estimate it would take at least 7 hours,
+;;;and possibly several days.
+;;;Which means I would have still gotten an answer before Christmas
 #lang racket
 (require queue)
 
@@ -34,11 +42,11 @@
 ;;Includes division by 3
 (define (operation str)
   (let ((parsed (string-split str)))
-    (λ (old) (quotient ((if (string=? (second parsed) "*") * +)
-                        old
-                        (if (string=? (third parsed) "old")
-                            old
-                            (string->number (third parsed)))) 3))))
+    (λ (old) ((if (string=? (second parsed) "*") * +)
+              old
+              (if (string=? (third parsed) "old")
+                  old
+                  (string->number (third parsed)))))))
 
 ;;Generates a procedure from the three Test lines in today's input
 ;;This procedure will take a worry level as input and return a monkey's number
@@ -52,7 +60,7 @@
 ;;Returns a vector containing each monkey's activity level after n rounds
 ;;This function uses mutable vectors and for-each internally,
 ;;but does not depend on any external state
-(define (run-n-rounds monkeys rounds)
+(define (run-n-rounds monkeys rounds reduce reduce-val)
   (let ((queues (list->vector (map list->queue (map first monkeys))))
         (inspects (list->vector (map second monkeys)))
         (tests (list->vector (map third monkeys)))
@@ -61,7 +69,7 @@
       (cond ((queue-empty? (vector-ref queues n)) (void))
             (else
              (define-values (elem new-q) (queue-remove (vector-ref queues n)))
-             (let* ((new-val ((vector-ref inspects n) elem))
+             (let* ((new-val (reduce ((vector-ref inspects n) elem) reduce-val))
                     (new-idx ((vector-ref tests n) new-val)))
                (vector-set!
                 queues
@@ -88,4 +96,13 @@
        (vector->list
         (vector-take
          (vector-sort
-          (run-n-rounds input 20) >) 2)))
+          (run-n-rounds input 20 quotient 3) >) 2)))
+
+;I'm using a hard-coded mod value here.
+;TODO: write a function to find the mod value by going through the file again
+(display "Part 2: ")
+(foldl * 1
+       (vector->list
+        (vector-take
+         (vector-sort
+          (run-n-rounds input 10000 modulo 9699690) >) 2)))
