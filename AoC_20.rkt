179012eca79d3334c80c1ba06bce8c921e0be352
mquig42 <mquig42@users.noreply.github.com>
2022-12-20 23:46:05 -0500

    Day 20 both parts

--- AoC_20.rkt
+++ AoC_20.rkt
@@ -1,3 +1,10 @@
+;;;AoC_20.rkt
+;;;2022-12-20
+;;;Mike Quigley
+
+;;;Getting the movement logic to work here took me too long to get right.
+;;;Off-by-one errors in the list indexing.
+;;;Once I solved part 1, part 2 took less than 10 minutes.
 #lang racket
 
 ;;How did I not know about file->lines? I've been implementing my own version
@@ -11,9 +18,11 @@
 
 ;;Getters
 (define (value node)
-  (let ((v (cdr node)))
-    (cond ((> v 0) (+ v 1))
-          (else v))))
+  (cdr node))
+
+;;Applies decryption key to a node
+(define (decrypt node)
+  (cons (car node) (* (cdr node) 811589153)))
 
 ;;Search lst for v, return its index
 (define (search lst v)
@@ -22,22 +31,26 @@
         (iter (cdr lst) v (+ i 1))))
   (iter lst v 0))
 
+;;Find the index of the node with 0 value
 (define (search-zero lst)
   (define (iter lst i)
     (if (= (cdr (car lst)) 0) i
         (iter (cdr lst) (+ i 1))))
   (iter lst 0))
 
+;;Moves a single item in the list
 (define (move item lst)
-  (let ((split-point (modulo (+ (value item) (search lst item))
-                             (length lst))))
-    (append (remove item (take lst split-point))
+  (let* ((idx (+ (search lst item) (value item)))
+         (tmp-lst (remove item lst))
+         (split-point (modulo idx (length tmp-lst))))
+    (append (take tmp-lst split-point)
             (list item)
-            (remove item (drop lst split-point)))))
+            (drop tmp-lst split-point))))
 
+;;moves all items in the list once
+;;Takes the same list as input twice. The first is used for instructions,
+;;the second for intermediate storage
 (define (move-all lst acc)
-  (display (map cdr acc))
-  (display "\r")
   (if (null? lst) acc
       (move-all (cdr lst) (move (car lst) acc))))
 
@@ -47,7 +60,24 @@
        (cdr (list-ref lst (modulo (+ zero 2000) (length lst))))
        (cdr (list-ref lst (modulo (+ zero 3000) (length lst)))))))
 
-(define input (index-lst (read-input "Test20.txt")))
-(define mixed-once (move-all input input))
+(define input (index-lst (read-input "Input20.txt")))
 
+(display "Part 1: ")
+(define mixed-once (move-all input input))
 (calc-coords mixed-once)
+
+(display "Part 2: ")
+(define decrypted-input (map decrypt input))
+;Too lazy to use a counter. Just repeat the process by repeating the list.
+(define mixed-ten (move-all (append decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input
+                                    decrypted-input)
+                            decrypted-input))
+(calc-coords mixed-ten)
