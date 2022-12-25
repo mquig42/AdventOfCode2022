77858f7664d20292c5565b7159bcdc2973518a78
mquig42 <mquig42@users.noreply.github.com>
2022-12-25 12:43:18 -0500

    Day 25

--- AoC_25.rkt
+++ AoC_25.rkt
@@ -12,7 +12,8 @@
 ;;;the smallest possible number of that length, so almost certainly too small.
 ;;;Replace the first digit with, in sequence, -, 0, 1, 2. If the SNAFU value
 ;;;of the string is > n, you've gone too far so step back one.
-;;;For example, if "1====" < n and "2====" > n, then the first digit must be 1
+;;;For example, if "1====" < n and "2====" > n, then the first digit must be 1.
+;;;Or, to rephrase: find the largest digit that still makes the string < n.
 ;;;Once you figure out the value of a digit, move on to the next one. Keep going
 ;;;until you find a string that evaluates to n.
 
