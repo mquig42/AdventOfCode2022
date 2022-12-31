edb390b86114b44e217df04c8c420827e2cc566f
mquig42 <mquig42@users.noreply.github.com>
2022-12-31 16:22:32 -0500

    Day 17 part 1

--- AoC_17.rkt
+++ AoC_17.rkt
@@ -2,73 +2,118 @@
 ;;;2022-12-17
 ;;;Mike Quigley
 
-;;;I cheated and went to the subreddit before even starting this.
-;;;This means I know that part 2 is one of those "run part 1 a zillion times"
-;;;scenarios.
+;;;Decided to represent the board with lists of 1s and 0s. Each level is a list
+;;;of 7 values, and the overall structure is a list of levels. I add new levels
+;;;to the head.
 
-;;;Could be a repeating pattern. There are 5 rock types, and my input length
-;;;is 10091. Both of those are prime numbers. Interestingly, the sample length
-;;;is 40, which is not prime
-
-;;;I'll probably need to do the actual simulation as a basis, then extrapolate
-;;;results.
-
-;;;For coordinates, (0 . 0) represents the bottom-left of the chamber
-
-;;;Update: I've been doing this completely wrong. Don't use a set of points.
-;;;This is a situation where a grid of nested lists are better. That gives
-;;;stack-like behaviour. It also makes it easy to select a given y-level for
-;;;collision detection. Set intersections would work too, but you'd need to
-;;;iterate through the whole set.
-
+;;;The real challenge is part 2. I wrote a simple hash function, ran it on the
+;;;entire board after dropping 10,000 rocks, and dumped the results into Gnuplot
+;;;The board has a repeating pattern of length 2642, which is a lot shorter
+;;;than I was expecting. I should be able to exploit that to skip a lot
+;;;of steps. 
 #lang racket
 
 (define (read-input file)
-  (map (λ (x) (if (eq? x #\<) -1 1))
+  (map (λ (x) (if (eq? x #\<) '(0 . -1) '(0 . 1)))
        (string->list (string-trim (read-line file)))))
 
-(define (make-point x y)
-  (cons x y))
-(define (get-x point)
-  (car point))
-(define (get-y point)
-  (cdr point))
-(define (point-add a b)
-  (make-point (+ (get-x a) (get-x b))
-              (+ (get-y a) (get-y b))))
+;;Coord getters
+(define get-row car)
+(define get-col cdr)
+(define make-coord cons)
+(define (add-coords a b)
+  (make-coord (+ (get-row a) (get-row b))
+              (+ (get-col a) (get-col b))))
+
+;;Lookup position on board
+(define (board-ref b coord)
+  (list-ref (list-ref b (get-row coord)) (get-col coord)))
+
+;;Sets given coord to 1
+(define (board-set b coord)
+  (list-set b (get-row coord)
+            (list-set (list-ref b (get-row coord)) (get-col coord) 1)))
 
 ;;I have a feeling I'll be using this a lot
 (define (inc-mod x m)
   (modulo (+ x 1) m))
 
-;;Makes a set of points
-(define (rock->set rock offset)
-  (list->set (map (λ (r) (point-add r offset)) rock)))
+;;Trims empty levels off the top of the board
+(define (board-trim b)
+  (if (equal? (car b) '(0 0 0 0 0 0 0))
+      (board-trim (cdr b))
+      b))
 
-;;Checks if a rock is outside the walls
-(define (oob? rock x-offset)
-  (or (< (+ (get-x (first rock)) x-offset) 0)
-      (> (+ (get-x (second rock)) x-offset) 6)))
+;;Adds n empty levels to the top of board
+(define (add-levels b n)
+  (if (= n 0) b
+      (add-levels (cons '(0 0 0 0 0 0 0) b) (- n 1))))
 
-;;stack is a set of points. Returns maximum y value of those points
-;;Might not want to use this. It takes linear time
-(define (max-height stack)
-  (argmax identity (map get-y (set->list stack))))
+;;Adds a rock to the board at given coords
+(define (add-rock rock coord b)
+  (if (null? rock) b
+      (add-rock (cdr rock) coord (board-set b (add-coords (car rock) coord)))))
 
-(define input-file (open-input-file "Test17.txt"))
-(define input (read-input input-file))
+;;Returns true if rock can move into given coords, false if it can't
+(define (can-move? rock coord b)
+  (cond ((null? rock) true)
+        ((< (+ (get-col (car rock)) (get-col coord)) 0) false)
+        ((> (+ (get-col (car rock)) (get-col coord)) 6) false)
+        ((= (board-ref b (add-coords (car rock) coord)) 1) false)
+        (else (can-move? (cdr rock) coord b))))
+
+;;Each iteration of this function runs one sim cycle: that is,
+;;one move across and one move down. If it's not possible to move down,
+;;it enlarges the board and moves on to the next rock.
+;;To make room for a new rock, add 7 levels and start it at (3 . 2)
+(define (run-sim b rock-id vent-id coord rock-counter goal)
+  (if (= rock-counter goal) b
+      (let* ((v-coord (add-coords coord (vector-ref input vent-id)))
+             (new-col (if (can-move? (rocks rock-id) v-coord b)
+                          v-coord
+                          coord))
+             (new-row (add-coords new-col '(1 . 0)))
+             (new-vent (inc-mod vent-id (vector-length input))))
+        (if (can-move? (rocks rock-id) new-row b)
+            (run-sim b rock-id new-vent new-row rock-counter goal)
+            (run-sim
+             (add-levels
+              (board-trim
+               (add-rock (rocks rock-id) new-col b)) 7)
+             (inc-mod rock-id 5)
+             new-vent
+             '(3 . 2)
+             (+ rock-counter 1)
+             goal)))))
+
+;;Generates a unique integer representation of a filled level
+;;Since a level is a list of 1s and 0s, just convert it from binary to decimal.
+(define (hash-level level)
+  (define (iter lst p2 acc)
+    (if (null? lst) acc
+        (iter (cdr lst) (/ p2 2) (+ acc (* (car lst) p2)))))
+  (iter level 64 0))
+                         
+
+(define input-file (open-input-file "Input17.txt"))
+(define input (list->vector (read-input input-file)))
 (close-input-port input-file)
 
-;;Hard-code the shapes and put them in a list
-;;Each rock has its minimum x-value in the first point and the max in the second
-;;The first x value is always 2 so x offsets can start at 0
-(define r1 '((2 . 0) (5 . 0) (3 . 0) (4 . 0)))
-(define r2 '((2 . 1) (4 . 1) (3 . 0) (3 . 1) (3 . 2)))
-(define r3 '((2 . 0) (4 . 0) (3 . 0) (4 . 1) (4 . 2)))
-(define r4 '((2 . 0) (2 . 1) (2 . 2) (2 . 3)))
-(define r5 '((2 . 0) (3 . 0) (2 . 1) (3 . 1)))
-(define rocks (list r1 r2 r3 r4 r5))
+;Starting condition
+(define board (list (list 1 1 1 1 1 1 1)))
 
-(define cave-floor
-  (set '(0 . 0) '(1 . 0) '(2 . 0) '(3 . 0) '(4 . 0) '(5 . 0) '(6 . 0)))
+;Rocks
+(define r1 '((0 . 0) (0 . 1) (0 . 2) (0 . 3)))
+(define r2 '((0 . 1) (-1 . 0) (-1 . 1) (-1 . 2) (-2 . 1)))
+(define r3 '((0 . 0) (0 . 1) (0 . 2) (-1 . 2) (-2 . 2)))
+(define r4 '((0 . 0) (-1 . 0) (-2 . 0) (-3 . 0)))
+(define r5 '((0 . 0) (0 . 1) (-1 . 0) (-1 . 1)))
+(define (rocks r)
+  (cond ((= r 0) r1)
+        ((= r 1) r2)
+        ((= r 2) r3)
+        ((= r 3) r4)
+        ((= r 4) r5)))
 
+(display "Part 1: ")
+(- (length (board-trim (run-sim (add-levels board 7) 0 0 '(3 . 2) 0 2022))) 1)
